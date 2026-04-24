eee6c30918b9b0ec76fe37068f119876f41000ff
Zarina Ospanova <darinaospanova@Noutbuk-darina.local>
2026-04-24 20:59:07 +0500

    add create account page

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink], // 🔥 ВАЖНО!
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-  constructor(public authService: AuthService) {}
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+  ) {}
+
+  goTo(path: string): void {
+    this.router.navigate([path]);
+  }
 
   logout(): void {
     this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
