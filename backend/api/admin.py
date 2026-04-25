a3fd60d906fd02dcafc7b45f1d10f83fb0c85e14
miclickk <askar050307@gmail.com>
2026-04-25 05:34:44 +0500

    Fixed admin panel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,19 +1,18 @@
 from django.contrib import admin
-from .models import Category, Post, Comment
-
+from .models import Category, ItemPost, Comment
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
     search_fields = ['name']
 
-@admin.register(Post)
+@admin.register(ItemPost)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'status', 'author', 'category', 'created_at']
+    list_display = ['id', 'title', 'status', 'user', 'category', 'created_at']
     list_filter = ['status', 'category']
     search_fields = ['title', 'description']
     readonly_fields = ['created_at', 'updated_at']
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'post', 'author', 'created_at']
+    list_display = ['id', 'post', 'user', 'created_at']
     search_fields = ['text']
